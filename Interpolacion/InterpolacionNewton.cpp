7fb6a1453e9055372c35c0c4539a0f352ae16824
Eduardo Merino <A01630088@itesm.mx>
2017-05-05 15:02:14 -0500

    Interpolacion

--- Interpolacion/InterpolacionNewton.cpp
+++ Interpolacion/InterpolacionNewton.cpp
@@ -3,30 +3,52 @@
 
 using namespace std;
 
-double diferenciaDivididaFinita(double x0, double fx0, double x1, double fx1){
-  return (fx1 - fx0) / (x1 -x0);
+double diferenciaDivFinita(int i0, int i1, double x[], double y[]) {
+
+	if (i1 - i0 == 0) {
+		return y[i0];
+	}
+	else {
+		return (diferenciaDivFinita(i0 + 1, i1, x, y) -
+			diferenciaDivFinita(i0, i1 - 1, x, y)) / (x[i1] - x[i0]);
+	}
 }
 
-double valorNecesario = 0.0;
+void Newton(double x[], double y[], int tamaño, double valor) {
 
-*double calculaColumna(double[] fx, int n){
-  double *columna = new double[n - 1];
-  //for
+	double aproximacion = 0;
+	double *divisiones = new double[tamaño];
+
+	cout << "Coeficientes: " << endl;
+
+	for (int i = 0; i < tamaño; i++) {
+		divisiones[i] = diferenciaDivFinita(0, i, x, y);
+		cout << divisiones[i] << endl;
+	}
+
+
+	for (int i = 0; i < tamaño; i++) {
+		double aprox = divisiones[i];
+
+		for (int j = 0; j < i; j++) {
+			aprox *= (valor - x[j]);
+		}
+
+		aproximacion += aprox;
+	}
+
+	cout << "Valor aproximado de " << valor << " es: " << aproximacion << '\n';
 }
 
-void interpolacionNewton (double x[], double y[], int n){
+int main() {
 
-std::cout << y[0] <<" + " <<;
+	double x[] = { 1, 4, 6 };
+	double y[] = { 0, 1.386294, 1.791770 };
 
-}
+  double z = 0.0;
+  cout << "Dame un número: ";
+  cin >> z;
 
-int main(int argc, char const *argv[]) {
-  double x[] = {1, 4, 6};
-  double fx[] = {0, 1.386294, 1.791759};
+	Newton(x, y, sizeof(x)/sizeof(x[0]), z);
 
-  int n = (sizeof(x)/sizeof(x[0]);
-
-  interpolacionNewton(x, fx, n);
-
-  return 0;
 }
